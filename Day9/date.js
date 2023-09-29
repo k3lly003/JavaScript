d904b3f54b6db7f08142a3f908f694bce19d5ee9
k3lly003 <ntirukelly@gmail.com>
2023-09-29 18:55:36 +0300

    initial commit

---
+++ Day9/date.js
@@ -0,0 +1,12 @@
+//Javascript Date methods
+//constructors
+const date = new Date();
+console.log(date); //Thu Jan 01 2022 13:56:47 GMT+08
+
+new Date(1682452849453);
+
+new Date("2023-04-25");
+
+new Date("2023-04-25T01:10:00");
+
+new Date("2023, 3, 25, 1, 0, 0, 0");
