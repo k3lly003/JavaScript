996f917f1060db246032670d7fc98c653607d1d8
k3lly003 <ntirukelly@gmail.com>
2023-10-03 22:28:42 +0300

    initial commit

---
+++ Day10/Condition.js
@@ -0,0 +1,27 @@
+//SWITCH CASE
+let favLanguage = 3;
+let LangName;
+
+switch (
+  favLanguage // favLanguage is EXPRESSION
+) {
+  case 1:
+    LangName = "Java";
+    break;
+  case 2:
+    LangName = "C"; // all the langName are "SCENARIOS"
+    break;
+  case 3:
+    LangName = "JavaScript"; // The switch statement compares an expression against a set of
+    break; // dft scenarios and executes code based on the matching cases
+  case 4:
+    LangName = "Go";
+    break;
+  case 5:
+    LangName = "Python"; //executes when CASE == EXPRESSION
+    break;
+  default:
+    LangName = "Unknown";
+    break;
+}
+console.log("Your favorite language is " + LangName);
