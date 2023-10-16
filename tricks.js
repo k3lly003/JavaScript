34a2b392c34f1145ead2a134c59dc40141b3023d
k3lly003 <ntirukelly@gmail.com>
2023-10-16 16:51:33 +0200

    inital commit

--- tricks.js
+++ tricks.js
@@ -1,8 +1,44 @@
 //Preventing User from pasting
 
-Input.addEventListener("paste", (e) => {
-  e.preventDefault();
-});
+// Input.addEventListener("paste", (e) => {
+//   e.preventDefault();
+// });
 
-//Use Window.print() Javascript Method to print the content of current page
-<button onClick="window.print()">Print</button>;
+// //Use Window.print() Javascript Method to print the content of current page
+// <button onClick="window.print()">Print</button>;
+
+// let a = [1, 5, 42, 56, 77];
+
+// let solution = a.sort((a, b) => b - a);
+// console.log(solution);
+// Map.set()
+// const fruits =new.Map([
+//   ['🍉', 100],
+//   ['🍌', 50],
+//   ['🥭',250],
+//   ['🍏',apple],
+//   ['🍋',lemon],
+// ])
+const wallet = {
+  Balance: 5000,
+};
+const menu = {
+  pizza: {
+    price: 1000,
+  },
+  chiz: {
+    price: 2000,
+  },
+};
+function leti() {
+  if (menu.pizza.money == 100) {
+    Balance - menu.pizza.money;
+    console.log("🍕");
+  } else if (menu.chiz.money == 2000) {
+    Balance - menu.chiz.money;
+    console.log("🍟");
+  } else {
+    console.log("Bro u broke 😂");
+  }
+}
+let sellerOutPut = setTimeout(leti, 5000);
