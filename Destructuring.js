b8f461517b121f1ad1b2b93a89a9b70fdbd5ed20
k3lly003 <ntirukelly@gmail.com>
2023-10-26 16:48:47 +0200

    initial commit destructuring

---
+++ Destructuring.js
@@ -0,0 +1,14 @@
+/*Destructuring is a feature  introduced in ES6 (ES2015)    that provides a way to extract 
+values from arrays or properties from objects and bind them to distinct variables.
+It allows for more concise and readable code when accessing these values*/
+//
+/*Let's break it down 👇🏿*/
+/*ARRAY DESTRUCTURING*/
+//
+/*With array destructuring you can
+unpack items from array directly into valiables*/
+const colors = ["red", "green", "blue"];
+const [firstColor, secondColor] = colors;
+/*Output*/
+console.log(firstColor);
+console.log(secondColor);
