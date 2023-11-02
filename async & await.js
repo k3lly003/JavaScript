8be535c5686b1ddfe11fe32bcca30359fec94fe4
k3lly003 <ntirukelly@gmail.com>
2023-11-02 16:15:45 +0100

    initial commit

---
+++ async & await.js
@@ -0,0 +1,11 @@
+/*Used async to make the function act asynchronous*/
+/*async getData(){
+    try{
+        /*Used await to make the code wait until promise returns a result*/ /*
+        const res = await fetch('URL')
+        const data = await res.json()
+        return dta
+    }catch (err){
+        console.log(err)
+    }
+}*/
