25cbec56d085411624185db2873b0a0d15ece521
k3lly003 <ntirukelly@gmail.com>
2023-11-07 14:02:06 +0200

    initial commit

--- day8/index.js
+++ day8/index.js
@@ -20,13 +20,7 @@ document.getElementById("java").innerHTML = "JavaScript is good";
 document.getElementById("p1").style["color"] = "green";
 document.getElementById("p1").style["padding-top"] = "15px";
 
-// console.log("Brice");
-// let a = new date("2020-04-06");
-// document.getElementById("rush").innerHTMLFullYear();
-
-// //
-
-//to create a tag in J.S
+/*to create a tag in J.S*/
 const test = document.createElement("h3");
 test.id = "test";
 test.innerHTML = "Helo world";
@@ -34,11 +28,11 @@ test.innerHTML = "Helo world";
 const p1 = document.createElement("p");
 p1.id = "p1";
 p1.innerHTML = "Today are learning practical Javascript";
-// //
-// const hash = document.createElement("p");
-// hash.id = "hash";
-// hash.innerHTML = "#learnProgress";
-// //
+//
+const hash = document.createElement("p");
+hash.id = "hash";
+hash.innerHTML = "#learnProgress";
+//
 const java = document.createElement("h1");
 java.id = "java";
 java.innerHTML = "JavaScript is powerful";
@@ -48,17 +42,17 @@ const abtlink = document.createElement("a");
 abtlink.id = "abtlink";
 abtlink.href = "about.html";
 abtlink.innerHTML = "about";
-// //
+//
 const cntlink = document.createElement("a");
 cntlink.id = "cntlink";
 cntlink.href = "contact.html";
 cntlink.innerHTML = "contact";
-// //
+//
 const link = document.createElement("a");
 link.id = "link";
 link.href = "www.instagram.com";
 link.innerHTML = "Instagram";
-// //
+//
 const rush = document.createElement("p");
 rush.id = "rush";
 rush.innerHTML = "date of today";
@@ -73,35 +67,29 @@ main.appendChild(cntlink);
 main.appendChild(link);
 document.body.appendChild(main);
 document.body.appendChild(rush);
-// //
-// const today = new date();
-// let today = document.getElementById("p");
-// today.id = "today";
-// today.innerHTML = "new date";
-//
+/*Simple Work 1*/
 const day = new Date();
 let p = document.getElementById("black");
 p.innerHTML = day;
-//
-
+/*work 2*/
 let birthdate = new Date("2002-12-03");
 p.innerHTML = birthdate;
-//
+/*work (GET DATE)*/
 let date = birthdate.getDay();
 p.innerHTML = date;
-//
+/*work (GET MONTH)*/
 let month = birthdate.getMonth() + 1;
 p.innerHTML = month;
-//
+/*work (GET FULL YEAR)*/
 let year = birthdate.getFullYear();
 p.innerHTML = year;
-//
+/*work (GET HOURS)*/
 let hour = birthdate.getHours();
 p.innerHTML = hour;
-// //
-// let minutes = birthdate.getMinutes();
-// p.innerHTML = minutes;
-//
+/*work (GET MINUTES)*/
+let minutes = birthdate.getMinutes();
+p.innerHTML = minutes;
+/*Getting the URL that you have opened in the browser*/
 let website = window.location.href;
 p.innerHTML = website;
 // to see the protocol of your web
