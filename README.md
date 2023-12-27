820b4e7f344bab378162d88e611fe74ce1df06b9
k3lly003 <ntirukelly@gmail.com>
2023-12-27 12:54:34 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # JavaScript
 
-Here in this repo you find many JavaScript concepts that so helpful both for beginner and senior dev
+Here in this repo you find many JavaScript concepts that are so helpful both for beginner and senior dev
 
 # Prerequiries
 
-To be able to understand this concepts it only requires time patience, it's true that it take time to understand
+To be able to understand this concepts it only requires time and patience, it's true that it take time to understand
 some concepts but it's fine to not understand them now, you have the whole time in the future to view them,
 again practice makes perfect so I recomend that you practice as many times as you can.
 
--- day12/consDest/Destructuring.js
+++ day12/consDest/Destructuring.js
@@ -40,3 +40,56 @@ function introduce({ nam }) {
   console.log(`My name is ${nam}`);
 }
 introduction(profile); //My name is code
+//Destructuring
+//Taking a value from arrays or properties of an object and set them as local valiables
+
+//Old way😔
+const bigCats = ["🦁", "🐯"];
+const tiger = arr["🐯"];
+const lion = arr["🦁"];
+
+//New way😎
+const [egg, fish] = ["🥚", "🐟"];
+
+//the synthax looks the same as that of useState in react
+const [num, setNum] = useState(0);
+//
+const arr = ["🥓", "🍉", "🍟"];
+const [bacon, waterMellon, fries] = food;
+
+const obj = {
+  banana: "🍌",
+  bugger: "🍔",
+  pizza: "🍕",
+};
+const { banana, ...rest } = obj;
+
+console.log(rest);
+
+//Destructuring can also work on Nested obj
+
+const object = {
+  banana: "🍌",
+  bugger: "🍔",
+  pizza: "🍕",
+  parent: {
+    child: "👶",
+  },
+};
+const {
+  parent: { child },
+} = object;
+
+console.log(child);
+
+//U can ignore Values
+
+const cars = {
+  van: "🚐",
+  truck: "🚛",
+  bus: "🚎",
+  ambulance: "🚑",
+};
+const [, , , ambulance] = car;
+
+console.log(car);
